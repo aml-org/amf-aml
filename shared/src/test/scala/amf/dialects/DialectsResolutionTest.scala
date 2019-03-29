47f75beae04663b3807c5d38abaf2466af8085d0
Hernan Najles <hernan.naj@gmail.com>
2019-03-29 18:01:57 -0300

    Finish with tests

--- shared/src/test/scala/amf/dialects/DialectsResolutionTest.scala
+++ shared/src/test/scala/amf/dialects/DialectsResolutionTest.scala
@@ -9,20 +9,20 @@ import scala.concurrent.ExecutionContext
 
 
 
-class DialectsResolutionTest extends DialectInstanceResolutionTest {
+class DialectsResolutionTest extends DialectInstanceResolutionCycleTests {
   override implicit val executionContext: ExecutionContext = ExecutionContext.Implicits.global
 
   override val basePath = "shared/src/test/resources/vocabularies2/dialects/"
 
   test("resolve include test") {
-    cycle("example9.raml", "example9.resolved.raml", VocabularyYamlHint, Aml)
+    init().flatMap( _ => cycle("example9.raml", "example9.resolved.raml", VocabularyYamlHint, Aml))
   }
 
   test("resolve library test") {
-    cycle("example7.raml", "example7.resolved.raml", VocabularyYamlHint, Aml)
+    init().flatMap( _ => cycle("example7.raml", "example7.resolved.raml", VocabularyYamlHint, Aml))
   }
 
   test("resolve 13 test") {
-    cycle("example13.raml", "example13.resolved.raml", VocabularyYamlHint, Aml)
+    init().flatMap( _ => cycle("example13.raml", "example13.resolved.raml", VocabularyYamlHint, Aml))
   }
 }
--- shared/src/test/scala/amf/dialects/VocabularyParsingTest.scala
+++ shared/src/test/scala/amf/dialects/VocabularyParsingTest.scala
@@ -11,50 +11,50 @@ class VocabularyParsingTest extends DialectTests {
   val basePath = "shared/src/test/resources/vocabularies2/vocabularies/"
 
   test("parse 1 test") {
-    cycle("example1.raml", "example1.json", VocabularyYamlHint, Amf)
+    init().flatMap(_ => cycle("example1.raml", "example1.json", VocabularyYamlHint, Amf))
   }
 
   test("parse 2 test") {
-    cycle("example2.raml", "example2.json", VocabularyYamlHint, Amf)
+    init().flatMap(_ => cycle("example2.raml", "example2.json", VocabularyYamlHint, Amf))
   }
 
   test("parse 3 test") {
-    cycle("example3.raml", "example3.json", VocabularyYamlHint, Amf)
+    init().flatMap(_ => cycle("example3.raml", "example3.json", VocabularyYamlHint, Amf))
   }
 
   test("parse 4 test") {
-    cycle("example4.raml", "example4.json", VocabularyYamlHint, Amf)
+    init().flatMap(_ => cycle("example4.raml", "example4.json", VocabularyYamlHint, Amf))
   }
 
   test("parse 5 test") {
-    cycle("example5.raml", "example5.json", VocabularyYamlHint, Amf)
+    init().flatMap(_ => cycle("example5.raml", "example5.json", VocabularyYamlHint, Amf))
   }
 
   test("parse 6 test") {
-    cycle("example6.raml", "example6.json", VocabularyYamlHint, Amf)
+    init().flatMap(_ => cycle("example6.raml", "example6.json", VocabularyYamlHint, Amf))
   }
 
   test("generate 1 test") {
-    cycle("example1.json", "example1.raml", AmfJsonHint, Aml)
+    init().flatMap(_ => cycle("example1.json", "example1.raml", AmfJsonHint, Aml))
   }
 
   test("generate 2 test") {
-    cycle("example2.json", "example2.raml", AmfJsonHint, Aml)
+    init().flatMap(_ => cycle("example2.json", "example2.raml", AmfJsonHint, Aml))
   }
 
   test("generate 3 test") {
-    cycle("example3.json", "example3.raml", AmfJsonHint, Aml)
+    init().flatMap(_ => cycle("example3.json", "example3.raml", AmfJsonHint, Aml))
   }
 
   test("generate 4 test") {
-    cycle("example4.json", "example4.raml", AmfJsonHint, Aml)
+    init().flatMap(_ => cycle("example4.json", "example4.raml", AmfJsonHint, Aml))
   }
 
   test("generate 5 test") {
-    cycle("example5.json", "example5.raml", AmfJsonHint, Aml)
+    init().flatMap(_ => cycle("example5.json", "example5.raml", AmfJsonHint, Aml))
   }
 
   test("generate 6 test") {
-    cycle("example6.json", "example6.raml", AmfJsonHint, Aml)
+    init().flatMap(_ => cycle("example6.json", "example6.raml", AmfJsonHint, Aml))
   }
 }
