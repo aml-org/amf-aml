20284ab392729a63014d854f22e08f933f4c3417
Nicolas Schejtman <nschejtman93@gmail.com>
2021-04-08 11:57:15 -0300

    Refactor dialect header matching

--- amf-aml/shared/src/main/scala/amf/plugins/document/vocabularies/model/document/Dialect.scala
+++ amf-aml/shared/src/main/scala/amf/plugins/document/vocabularies/model/document/Dialect.scala
@@ -1,12 +1,12 @@
 package amf.plugins.document.vocabularies.model.document
 
-import amf.core.metamodel.Obj
 import amf.core.model.StrField
 import amf.core.model.document.{BaseUnit, DeclaresModel, EncodesModel}
 import amf.core.model.domain.DomainElement
 import amf.core.parser.{Annotations, Fields}
 import amf.plugins.document.vocabularies.metamodel.document.DialectModel
 import amf.plugins.document.vocabularies.metamodel.document.DialectModel._
+import amf.plugins.document.vocabularies.model.document.kind.DialectInstanceDocumentKind
 import amf.plugins.document.vocabularies.model.domain.DocumentsModel
 import org.mulesoft.common.core._
 
@@ -43,6 +43,8 @@ case class Dialect(fields: Fields, annotations: Annotations)
 
   def isPatchHeader(h: String): Boolean = patchHeader == h.stripSpaces
 
+  def isInstanceHeader(h: String): Boolean = header == h.stripSpaces
+
   def fragmentHeaders: Seq[String] =
     Option(documents())
       .map(_.fragments().map(f => s"%${f.documentName().value().stripSpaces}/${header.stripPrefix("%")}"))
@@ -55,6 +57,30 @@ case class Dialect(fields: Fields, annotations: Annotations)
   def allHeaders: Seq[String] = Seq(header) ++ libraryHeader ++ fragmentHeaders ++ Seq(patchHeader)
 
   def meta: DialectModel.type = DialectModel
+
+  def documentKindFor(header: String): Option[DialectInstanceDocumentKind] = {
+    header match {
+      case h if isLibraryHeader(h)  => Some(kind.DialectInstanceLibrary)
+      case h if isPatchHeader(h)    => Some(kind.DialectInstancePatch)
+      case h if isFragmentHeader(h) => Some(kind.DialectInstanceFragment)
+      case h if isInstanceHeader(h) => Some(kind.DialectInstance)
+      case _                        => None
+    }
+  }
+
+  def acceptsHeader(header: String): Boolean = documentKindFor(header).isDefined
+
+  private[amf] def usesKeyPropertyMatching: Boolean = {
+    val usesKeyProperty = for {
+      documentsModel  <- Option(documents())
+      usesKeyProperty <- documentsModel.keyProperty().option()
+    } yield {
+      usesKeyProperty
+    }
+    usesKeyProperty.contains(true)
+  }
+
+  private[amf] def usesHeaderMatching: Boolean = !usesKeyPropertyMatching
 }
 
 object Dialect {
