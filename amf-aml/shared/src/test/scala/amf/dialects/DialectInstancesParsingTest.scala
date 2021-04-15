4460ee2f4883e6ec6017c3bfc7c890c039b98ce1
Nicolas Schejtman <nschejtman93@gmail.com>
2021-04-15 20:02:23 -0300

    APIMF-3033: add test

--- amf-aml/shared/src/test/scala/amf/dialects/DialectInstancesParsingTest.scala
+++ amf-aml/shared/src/test/scala/amf/dialects/DialectInstancesParsingTest.scala
@@ -998,6 +998,18 @@ trait DialectInstancesParsingTest extends DialectTests {
   }
 
 
+  test("Cyclic references") {
+    withDialect(
+      "dialect.yaml",
+      "instance.flattened.jsonld",
+      "instance.golden.flattened.jsonld",
+      AmfJsonHint,
+      target = Amf,
+      renderOptions = Some(RenderOptions().withFlattenedJsonLd.withPrettyPrint),
+      directory = s"$basePath/cyclic-references/"
+    )
+  }
+
   protected def withInlineDialect(source: String,
                                   golden: String,
                                   hint: Hint,
