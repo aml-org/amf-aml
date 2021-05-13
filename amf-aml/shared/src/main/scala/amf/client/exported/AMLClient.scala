92d0adc3e0188d0f7fa672aff692766fbd5bad68
Agustin Bettati <bettatiagustin@gmail.com>
2021-05-13 17:32:15 -0300

    APIMF-2995: exported interfaces for AMLConfiguration and AMLClient

---
+++ amf-aml/shared/src/main/scala/amf/client/exported/AMLClient.scala
@@ -0,0 +1,43 @@
+package amf.client.exported
+
+import scala.scalajs.js.annotation.{JSExportAll, JSExportTopLevel}
+import amf.client.environment.{AMLClient => InternalAMLClient}
+import amf.client.convert.VocabulariesClientConverter._
+import amf.client.interface.AMFGraphClient
+
+import scala.concurrent.ExecutionContext
+
+@JSExportAll
+class AMLClient private (private val _internal: InternalAMLClient) extends AMFGraphClient(_internal) {
+
+  private implicit val ec: ExecutionContext = _internal.getConfiguration.getExecutionContext
+
+  @JSExportTopLevel("AMLClient")
+  def this(configuration: AMLConfiguration) = {
+    this(new InternalAMLClient(configuration))
+  }
+
+  override def getConfiguration: AMLConfiguration = _internal.getConfiguration
+
+  /**
+    * parse a {@link amf.plugins.document.vocabularies.model.document.Dialect}
+    * @param url of the resource to parse
+    * @return a Future {@link amf.client.environment.AMLDialectResult}
+    */
+  def parseDialect(url: String): ClientFuture[AMLDialectResult] = _internal.parseDialect(url).asClient
+
+  /**
+    * parse a {@link amf.plugins.document.vocabularies.model.document.DialectInstance}
+    * @param url of the resource to parse
+    * @return a Future {@link amf.client.environment.AMLDialectInstanceResult}
+    */
+  def parseDialectInstance(url: String): ClientFuture[AMLDialectInstanceResult] =
+    _internal.parseDialectInstance(url).asClient
+
+  /**
+    * parse a {@link amf.plugins.document.vocabularies.model.document.Vocabulary}
+    * @param url of the resource to parse
+    * @return a Future {@link amf.client.environment.AMLVocabularyResult}
+    */
+  def parseVocabulary(url: String): ClientFuture[AMLVocabularyResult] = _internal.parseVocabulary(url).asClient
+}
