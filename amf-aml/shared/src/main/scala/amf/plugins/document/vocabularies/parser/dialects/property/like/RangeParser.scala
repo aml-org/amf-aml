8e5cada01667d3b61b77f690791ed538d4b9c919
Tomas Fernandez <tfernandez@mulesoft.com>
2021-05-20 17:19:27 -0300

    Added lexical for resolved nodeMapping range references

--- amf-aml/shared/src/main/scala/amf/plugins/document/vocabularies/parser/dialects/property/like/RangeParser.scala
+++ amf-aml/shared/src/main/scala/amf/plugins/document/vocabularies/parser/dialects/property/like/RangeParser.scala
@@ -1,13 +1,13 @@
 package amf.plugins.document.vocabularies.parser.dialects.property.like
 
 import amf.core.model.DataType
-import amf.core.model.domain.AmfScalar
-import amf.core.parser.{Annotations, ValueNode, YMapOps}
+import amf.core.model.domain.{AmfArray, AmfScalar}
+import amf.core.parser.{Annotations, ScalarNode, ValueNode, YMapOps}
 import amf.core.vocabulary.Namespace
 import amf.plugins.document.vocabularies.model.domain.PropertyLikeMapping
-import org.yaml.model.{YMap, YMapEntry, YType}
+import org.yaml.model.{YMap, YMapEntry, YSequence, YType}
 import amf.plugins.document.vocabularies.metamodel.domain.PropertyLikeMappingModel
-import amf.plugins.document.vocabularies.metamodel.domain.PropertyMappingModel.LiteralRange
+import amf.plugins.document.vocabularies.metamodel.domain.PropertyMappingModel.{LiteralRange, ObjectRange}
 
 case class RangeParser(map: YMap, propertyLikeMapping: PropertyLikeMapping[_ <: PropertyLikeMappingModel]) {
   def parse(): Unit = {
@@ -16,7 +16,8 @@ case class RangeParser(map: YMap, propertyLikeMapping: PropertyLikeMapping[_ <: 
         entry => {
           entry.value.tagType match {
             case YType.Seq =>
-              propertyLikeMapping.withObjectRange(entry.value.as[Seq[String]])
+              val scalars = entry.value.as[YSequence].nodes.map(ScalarNode(_).text())
+              propertyLikeMapping.set(ObjectRange, AmfArray(scalars, Annotations(entry.value)), Annotations(entry))
             case _ =>
               val value = ValueNode(entry.value)
               val range = value.string().toString
@@ -26,15 +27,18 @@ case class RangeParser(map: YMap, propertyLikeMapping: PropertyLikeMapping[_ <: 
                 case "string" | "integer" | "boolean" | "float" | "decimal" | "double" | "duration" | "dateTime" |
                     "time" | "date" | "anyType" =>
                   setLiteralRange((Namespace.Xsd + range).iri(), entry)
-                case "anyUri"  => setLiteralRange(DataType.AnyUri, entry)
-                case "link"    => setLiteralRange((Namespace.Shapes + "link").iri(), entry)
-                case "number"  => setLiteralRange(DataType.Number, entry)
-                case "uri"     => setLiteralRange(DataType.AnyUri, entry)
-                case "any"     => setLiteralRange(DataType.Any, entry)
-                case "anyNode" => propertyLikeMapping.withObjectRange(Seq((Namespace.Meta + "anyNode").iri()))
+                case "anyUri"      => setLiteralRange(DataType.AnyUri, entry)
+                case "link"        => setLiteralRange((Namespace.Shapes + "link").iri(), entry)
+                case "number"      => setLiteralRange(DataType.Number, entry)
+                case "uri"         => setLiteralRange(DataType.AnyUri, entry)
+                case "any"         => setLiteralRange(DataType.Any, entry)
+                case "anyNode"     => propertyLikeMapping.withObjectRange(Seq((Namespace.Meta + "anyNode").iri()))
                 case nodeMappingId =>
-                  propertyLikeMapping
-                    .withObjectRange(Seq(nodeMappingId)) // temporary until we can resolve all nodeMappings after finishing parsing declarations
+                  // temporary until we can resolve all nodeMappings after finishing parsing declarations
+                  propertyLikeMapping.set(
+                      ObjectRange,
+                      AmfArray(Seq(AmfScalar(nodeMappingId, Annotations(entry.value))), Annotations.virtual()),
+                      Annotations(entry))
               }
           }
         }
