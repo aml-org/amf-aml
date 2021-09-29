2dd0c4f82578b5d37b9e2f0f98f959b43cb0662e
Tomas Fernandez <tfernandez@mulesoft.com>
2021-09-29 16:58:46 -0300

    Fix: removed source spec annotation and lifted SourceSpec model field to amf-core

--- amf-aml/shared/src/main/scala/amf/aml/client/scala/model/document/DialectInstanceProcessingData.scala
+++ amf-aml/shared/src/main/scala/amf/aml/client/scala/model/document/DialectInstanceProcessingData.scala
@@ -3,8 +3,8 @@ import amf.aml.internal.metamodel.document.DialectInstanceProcessingDataModel
 import amf.aml.internal.metamodel.document.DialectInstanceProcessingDataModel._
 import amf.core.client.scala.model.StrField
 import amf.core.client.scala.model.document.BaseUnitProcessingData
-import amf.core.internal.metamodel.document.BaseUnitProcessingDataModel
 import amf.core.internal.parser.domain.{Annotations, Fields}
+import amf.core.internal.remote.{AmlDialectSpec, Spec}
 
 case class DialectInstanceProcessingData(override val fields: Fields, override val annotations: Annotations)
     extends BaseUnitProcessingData(fields, annotations) {
@@ -14,6 +14,8 @@ case class DialectInstanceProcessingData(override val fields: Fields, override v
   def withDefinedBy(dialectIri: String): DialectInstanceProcessingData = set(DefinedBy, dialectIri)
   def withGraphDependencies(dialectIris: Seq[String]): DialectInstanceProcessingData =
     set(GraphDependencies, dialectIris)
+
+  override protected[amf] def sourceSpecProvider: Option[Spec] = sourceSpec.option().map(AmlDialectSpec)
 
   override def meta: DialectInstanceProcessingDataModel.type = DialectInstanceProcessingDataModel
 }
--- amf-aml/shared/src/main/scala/amf/aml/internal/parse/vocabularies/VocabulariesParser.scala
+++ amf-aml/shared/src/main/scala/amf/aml/internal/parse/vocabularies/VocabulariesParser.scala
@@ -1,21 +1,19 @@
 package amf.aml.internal.parse.vocabularies
 
+import amf.aml.client.scala.model.document.Vocabulary
+import amf.aml.client.scala.model.domain._
+import amf.aml.internal.metamodel.document.VocabularyModel
+import amf.aml.internal.metamodel.domain.{ClassTermModel, ObjectPropertyTermModel}
+import amf.aml.internal.parse.common.{DeclarationKey, DeclarationKeyCollector}
 import amf.core.client.scala.model.DataType
 import amf.core.client.scala.model.document.BaseUnit
 import amf.core.client.scala.model.domain.{AmfArray, AmfScalar}
 import amf.core.client.scala.parse.document.SyamlParsedDocument
-import amf.core.internal.parser.{Root, YMapOps}
-import amf.core.internal.parser.domain.{Annotations, BaseSpecParser, DefaultArrayNode, ValueNode}
-import amf.aml.internal.metamodel.document.VocabularyModel
-import amf.aml.internal.metamodel.domain.{ClassTermModel, ObjectPropertyTermModel}
-import amf.aml.client.scala.model.document.Vocabulary
-import amf.aml.client.scala.model.domain._
-import amf.aml.internal.parse.common.{DeclarationKey, DeclarationKeyCollector}
-import org.yaml.model._
 import amf.core.client.scala.vocabulary.Namespace
-import amf.core.internal.annotations.SourceSpec
-import amf.core.internal.remote.Spec
+import amf.core.internal.parser.domain.{Annotations, BaseSpecParser, DefaultArrayNode, ValueNode}
+import amf.core.internal.parser.{Root, YMapOps}
 import amf.core.internal.remote.Spec.AML
+import org.yaml.model._
 
 class VocabulariesParser(root: Root)(implicit override val ctx: VocabularyContext)
     extends BaseSpecParser
@@ -85,7 +83,7 @@ class VocabulariesParser(root: Root)(implicit override val ctx: VocabularyContex
         }
     }
 
-    vocabulary.annotations += SourceSpec(AML)
+    vocabulary.processingData.withSourceSpec(AML)
     vocabulary
   }
 
