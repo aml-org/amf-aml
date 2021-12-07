25a06132e0a911a029407c7f55ed38846b7e4dfd
Agustin Bettati <bettatiagustin@gmail.com>
2021-12-07 17:01:45 -0300

    APIMF-3529: move generation of source information node from parsing to resolution

--- amf-aml/shared/src/main/scala/amf/aml/internal/transform/pipelines/DialectTransformationPipeline.scala
+++ amf-aml/shared/src/main/scala/amf/aml/internal/transform/pipelines/DialectTransformationPipeline.scala
@@ -3,11 +3,12 @@ package amf.aml.internal.transform.pipelines
 import amf.core.client.scala.errorhandling.AMFErrorHandler
 import amf.aml.internal.transform.steps.{DialectNodeExtensionStage, DialectReferencesResolutionStage}
 import amf.core.client.scala.transform.{TransformationPipeline, TransformationStep}
+import amf.core.internal.transform.stages.SourceInformationStage
 
 class DialectTransformationPipeline private (override val name: String) extends TransformationPipeline() {
 
   override def steps: Seq[TransformationStep] =
-    Seq(new DialectReferencesResolutionStage(), new DialectNodeExtensionStage())
+    Seq(new DialectReferencesResolutionStage(), new DialectNodeExtensionStage(), SourceInformationStage)
 
 }
 
