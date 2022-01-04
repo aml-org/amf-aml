6ba1d2e49723586a21ddac933f9b32b87ba8764e
Loose <alejandro.loose@mulesoft.com>
2022-01-04 18:06:26 -0300

    APIMF-3448: added model and classes of ConditionalNodeMapping

---
+++ amf-aml/shared/src/main/scala/amf/aml/client/scala/model/domain/ConditionalNodeMapping.scala
@@ -0,0 +1,39 @@
+package amf.aml.client.scala.model.domain
+
+import amf.aml.internal.metamodel.domain.ConditionalNodeMappingModel
+import amf.aml.internal.metamodel.domain.ConditionalNodeMappingModel._
+import amf.core.client.scala.model.StrField
+import amf.core.client.scala.model.domain.{DomainElement, Linkable}
+import amf.core.internal.parser.domain.{Annotations, Fields}
+import amf.core.internal.utils._
+import org.yaml.model.YMap
+
+case class ConditionalNodeMapping(fields: Fields, annotations: Annotations)
+    extends DomainElement
+    with Linkable
+    with MergeableMapping
+    with NodeMappable[ConditionalNodeMappingModel.type] {
+
+  override def meta: ConditionalNodeMappingModel.type = ConditionalNodeMappingModel
+
+  def ifMapping: StrField   = fields.field(If)
+  def thenMapping: StrField = fields.field(Then)
+  def elseMapping: StrField = fields.field(Else)
+
+  def withIfMapping(ifMapping: String): ConditionalNodeMapping     = set(If, ifMapping)
+  def withThenMapping(thenMapping: String): ConditionalNodeMapping = set(Then, thenMapping)
+  def withElseMapping(elseMapping: String): ConditionalNodeMapping = set(Else, elseMapping)
+
+  override def linkCopy(): ConditionalNodeMapping = ConditionalNodeMapping().withId(id)
+  override protected def classConstructor: (Fields, Annotations) => Linkable with DomainElement =
+    ConditionalNodeMapping.apply
+  private[amf] override def componentId: String = "/" + name.value().urlComponentEncoded
+}
+
+object ConditionalNodeMapping {
+  def apply(): ConditionalNodeMapping = apply(Annotations())
+
+  def apply(ast: YMap): ConditionalNodeMapping = apply(Annotations(ast))
+
+  def apply(annotations: Annotations): ConditionalNodeMapping = ConditionalNodeMapping(Fields(), annotations)
+}
