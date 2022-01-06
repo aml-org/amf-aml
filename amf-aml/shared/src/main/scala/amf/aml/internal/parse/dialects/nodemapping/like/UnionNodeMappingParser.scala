0914a74f84f8ccd728634501f314308b36cf1000
Loose <alejandro.loose@mulesoft.com>
2022-01-06 11:31:42 -0300

    APIMF-3448: extracted nodeMapping parsing logic. Added ConditionalMapping parsing

---
+++ amf-aml/shared/src/main/scala/amf/aml/internal/parse/dialects/nodemapping/like/UnionNodeMappingParser.scala
@@ -0,0 +1,69 @@
+package amf.aml.internal.parse.dialects.nodemapping.like
+
+import amf.aml.client.scala.model.domain.UnionNodeMapping
+import amf.aml.internal.metamodel.domain.UnionNodeMappingModel
+import amf.aml.internal.metamodel.domain.UnionNodeMappingModel.ObjectRange
+import amf.aml.internal.parse.dialects.DialectAstOps.{DialectScalarValueEntryParserOpts, DialectYMapOps}
+import amf.aml.internal.parse.dialects.DialectContext
+import amf.aml.internal.validate.DialectValidations.DialectError
+import amf.core.client.scala.model.domain.{AmfArray, AmfScalar, DomainElement}
+import amf.core.internal.parser.domain.Annotations
+import org.yaml.model.{YMap, YScalar, YSequence, YType}
+
+class UnionNodeMappingParser(implicit ctx: DialectContext) extends NodeMappingLikeParserInterface {
+
+  override def parse(map: YMap, adopt: DomainElement => Any, isFragment: Boolean): UnionNodeMapping = {
+    val unionNodeMapping = UnionNodeMapping(map)
+
+    adopt(unionNodeMapping)
+
+    map.key(
+        "union",
+        entry => {
+          entry.value.tagType match {
+            case YType.Seq =>
+              try {
+                val nodes = entry.value.as[YSequence].nodes.map(n => AmfScalar(n.as[String], Annotations(n)))
+                unionNodeMapping.set(ObjectRange, AmfArray(nodes, Annotations(entry.value)), Annotations(entry))
+              } catch {
+                case _: Exception =>
+                  ctx.eh.violation(DialectError,
+                                   unionNodeMapping.id,
+                                   s"Union node mappings must be declared as lists of node mapping references",
+                                   entry.value.location)
+              }
+            case _ =>
+              ctx.eh.violation(DialectError,
+                               unionNodeMapping.id,
+                               s"Union node mappings must be declared as lists of node mapping references",
+                               entry.value.location)
+          }
+        }
+    )
+
+    map.key(
+        "typeDiscriminator",
+        entry => {
+          val types = entry.value.as[YMap]
+          val typeMapping = types.entries.foldLeft(Map[String, String]()) {
+            case (acc, e) =>
+              val nodeMappingId = e.value.as[YScalar].text
+              acc + (e.key.as[YScalar].text -> nodeMappingId)
+          }
+          unionNodeMapping.withTypeDiscriminator(typeMapping, Annotations(entry), Annotations(types))
+        }
+    )
+
+    map.parse("typeDiscriminatorName", unionNodeMapping setParsing UnionNodeMappingModel.TypeDiscriminatorName)
+
+    unionNodeMapping
+  }
+
+}
+
+object UnionNodeMappingParser {
+
+  val identifierKey: String = "union"
+
+  def apply(implicit ctx: DialectContext) = new UnionNodeMappingParser
+}
