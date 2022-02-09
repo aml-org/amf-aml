47fb00a7948bef54de73e4eb6459808c799334ec
Loose <alejandro.loose@mulesoft.com>
2022-02-09 15:45:38 -0300

    Added ConditionalNodeMapping wrapper

--- amf-aml/shared/src/main/scala/amf/aml/internal/utils/VocabulariesRegister.scala
+++ amf-aml/shared/src/main/scala/amf/aml/internal/utils/VocabulariesRegister.scala
@@ -61,6 +61,9 @@ object VocabulariesRegister extends UniqueInitializer with PlatformSecrets {
     platform.registerWrapper(SemanticExtensionModel) {
       case s: domain.SemanticExtension => SemanticExtension(s)
     }
+    platform.registerWrapper(ConditionalNodeMappingModel) {
+      case s: domain.ConditionalNodeMapping => ConditionalNodeMapping(s)
+    }
     platform.registerWrapper(VocabularyModel) {
       case s: document.Vocabulary => new Vocabulary(s)
     }
